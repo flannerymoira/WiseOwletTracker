dc8868c7c9269c97793e648b6dedf2888bcc2e7d
Moira <Flannery-M4@ulster.ac.uk>
2020-03-30 16:35:36 +0100

    Add code to send email.

--- app/src/main/java/com/example/wiseowlettracker/StudentActivity.java
+++ app/src/main/java/com/example/wiseowlettracker/StudentActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -15,9 +16,38 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.AccessController;
+import java.security.Provider;
+import java.security.Security;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Properties;
+
+import javax.activation.DataSource;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Security;
+
+
 
 import static com.example.wiseowlettracker.DatabaseHelper.StudentEmail;
 import static com.example.wiseowlettracker.DatabaseHelper.StudentId;
@@ -78,7 +108,7 @@ public class StudentActivity extends AppCompatActivity {
         int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
         if (dayOfWeek == 2) {
             // Send Weekly email
-            sendEmail();
+            sendMail();
         }
     }
 
@@ -127,26 +157,180 @@ public class StudentActivity extends AppCompatActivity {
         return totalStudy;
     }
 
-    public void sendEmail() {
-        Log.i("Send email", "");
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
 
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, StudentEmail);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Wise Owlet Tracker Weekly Report");
-        emailIntent.putExtra(Intent.EXTRA_TEXT   , "Hello, " + StudentName + "." );
+    public void sendMail() {
+        try
+        {
+            LongOperation l=new LongOperation();
+            l.execute();  //sends the email in background
+            Toast.makeText(this, l.get(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
 
-        try {
-            if (emailIntent.resolveActivity(getPackageManager()) != null) {
-                startActivity(emailIntent);
-            }
-//            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-//            finish();
-            Log.i("Finished sending email", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(StudentActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public class GMailSender extends javax.mail.Authenticator
+    {
+        private String mailhost = "smtp.gmail.com";
+        private String user;
+        private String password;
+        private Session session;
+        private Multipart _multipart;
+
+        {
+            Security.addProvider(new JSSEProvider());
+        }
+
+        public GMailSender(String user, String password)
+        {
+            this.user = user;
+            this.password = password;
+
+            Properties props = new Properties();
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.host", mailhost);
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            props.setProperty("mail.smtp.quitwait", "false");
+
+            session = Session.getDefaultInstance(props, this);
+            _multipart = new MimeMultipart();
+        }
+
+        protected PasswordAuthentication getPasswordAuthentication()
+        {
+            return new PasswordAuthentication(user, password);
+        }
+
+        public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception
+        {
+            MimeMessage message = new MimeMessage(session);
+            DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));
+            message.setSender(new InternetAddress(sender));
+            message.setSubject(subject);
+
+            message.setDataHandler(handler);
+            message.setContent(_multipart);
+            if (recipients.indexOf(',') > 0)
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+            else
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
+            Transport.send(message);
+
+        }
+
+        public void addAttachment(String filename) throws Exception
+        {
+            BodyPart messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+
+            _multipart.addBodyPart(messageBodyPart);
+        }
+
+        public class ByteArrayDataSource implements DataSource {
+            private byte[] data;
+            private String type;
+
+            public ByteArrayDataSource(byte[] data, String type) {
+                super();
+                this.data = data;
+                this.type = type;
+            }
+
+            public ByteArrayDataSource(byte[] data) {
+                super();
+                this.data = data;
+            }
+
+            public void setType(String type)
+            {
+                this.type = type;
+            }
+
+            public String getContentType() {
+                if (type == null)
+                    return "application/octet-stream";
+                else
+                    return type;
+            }
+
+            public InputStream getInputStream() throws IOException
+            {
+                return new ByteArrayInputStream(data);
+            }
+
+            public String getName()
+            {
+                return "ByteArrayDataSource";
+            }
+
+            public OutputStream getOutputStream() throws IOException
+            {
+                throw new IOException("Not Supported");
+            }
+        }
+    }
+
+    public final class JSSEProvider extends Provider
+    {
+        private static final long serialVersionUID = 1L;
+
+        public JSSEProvider()
+        {
+            super("HarmonyJSSE", 1.0, "Harmony JSSE Provider");
+            AccessController.doPrivileged(new java.security.PrivilegedAction<Void>()
+            {
+                public Void run()
+                {
+                    put("SSLContext.TLS",
+                            "org.apache.harmony.xnet.provider.jsse.SSLContextImpl");
+                    put("Alg.Alias.SSLContext.TLSv1", "TLS");
+                    put("KeyManagerFactory.X509",
+                            "org.apache.harmony.xnet.provider.jsse.KeyManagerFactoryImpl");
+                    put("TrustManagerFactory.X509",
+                            "org.apache.harmony.xnet.provider.jsse.TrustManagerFactoryImpl");
+                    return null;
+                }
+            });
+        }
+    }
+
+    public class LongOperation extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                GMailSender sender = new GMailSender("moiraabyrne@gmail.com", "oldfield01");
+                sender.sendMail("This is a testing mail",
+                        "This is Body of testing mail","moiraabyrne@gmail.com",
+                        "moiraabyrne@gmail.com")                   ;
+            } catch (Exception e) {
+
+                Log.e("error", e.getMessage(), e);
+                return "Email Not Sent";
+            }
+            return "Email Sent";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e("LongOperation",result+"");
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
 }
 
